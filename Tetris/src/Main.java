32b4e390319566b44e5318b21429dceb142ae30a
HuynhSuThanh <thanh.hs274@gmail.com>
2019-10-27 22:06:34 +0700

    Thêm button

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -1,27 +1,42 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
 
-public class Main{
+public class Main implements ActionListener{
 	private JFrame frame;
-	JButton button;
+	JButton playButton, exitButton;
 	
 	public void JFrameDemo() {
 		createAndshow();
 	}
 	
 	public void createAndshow() {
-		button = new JButton("OK");
+		playButton = new JButton("Play");
+		exitButton = new JButton("Exit");
+		
 		frame = new JFrame("GodPen");
 		frame.setSize(400, 300);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(button);
 		frame.setLayout(new FlowLayout());
 		frame.setVisible(true);
+		
+		frame.add(playButton);
+		
+		frame.add(exitButton);
+		exitButton.setActionCommand("Exit");
+		exitButton.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if("Exit".equalsIgnoreCase(e.getActionCommand())) {
+			System.exit(0);
+		}
 	}
 	public static void main(String[] args) {
 		new Main().JFrameDemo();;
