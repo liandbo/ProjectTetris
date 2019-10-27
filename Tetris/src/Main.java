d2fb3e1a6aa89ca7cecba543fb5269ab992f099c
HuynhSuThanh <thanh.hs274@gmail.com>
2019-10-27 21:39:08 +0700

    thêm linh t

--- Tetris/src/Main.java
+++ Tetris/src/Main.java
@@ -1,4 +1,29 @@
+import java.awt.FlowLayout;
 
-public class Main {
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+
+
+public class Main{
+	private JFrame frame;
+	JButton button;
+	
+	public void JFrameDemo() {
+		createAndshow();
+	}
+	
+	public void createAndshow() {
+		button = new JButton("OK");
+		frame = new JFrame("GodPen");
+		frame.setSize(400, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(button);
+		frame.setLayout(new FlowLayout());
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new Main().JFrameDemo();;
+	}
 }
